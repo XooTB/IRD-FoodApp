cf67147c714446b9d0a3de08abacb7d9de24049e
Samiul Alim <xo.ot2001@gmail.com>
2023-11-07 22:25:55 +0600

    Fixed Alignment Issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import emptyImage from "../../assets/404.png";
 
 const page: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen mt-5 ml-5 rounded-tl-xl">
+    <div className="flex flex-col items-center justify-center w-full min-h-screen rounded-tl-xl">
       <Image src={emptyImage} alt="404" />
       <h1 className="text-xl font-bold">This page is Empty</h1>
     </div>
